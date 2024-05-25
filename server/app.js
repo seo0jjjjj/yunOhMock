49816fa56c671adc3d0548e0eba723c044311b62
= <seo0jjjjj@mail.com>
2024-05-25 16:44:33 +0900

    feat: create app.js

--- server/app.js
+++ server/app.js
@@ -1,13 +1,10 @@
 import express from "express";
 
+/**
+ * 미들웨어 설정, 라우팅 설정하는 파일
+ */
 const app = express();
-
-
 
 app.get("/",(req,res,next)=> {
   res.send("hello world!");
-})
-
-app.listen("8080",()=>{
-  console.log("app running");
 })
